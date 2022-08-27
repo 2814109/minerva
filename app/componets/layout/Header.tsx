3df18f6f9e00baef6bd6a3ccf582a0985fc032fa
kirke <kirke@kirkenoMacBook-Air.local>
2022-08-27 20:32:17 +0900

    create header

--- app/componets/layout/Header.tsx
+++ app/componets/layout/Header.tsx
@@ -1,4 +1,13 @@
 import { FC } from "react";
-const Header: FC = () => <h1>Header</h1>;
+const Header: FC = () => (
+  <div className=" bg-blue-600 text-white px-4 py-2">
+    <ul className="h-10 flex gap-10 items-center">
+      <li className="mr-5">Header Icon</li>
+      <li>Management</li>
+      <li>Library</li>
+      <li>Analysis</li>
+    </ul>
+  </div>
+);
 
 export default Header;
