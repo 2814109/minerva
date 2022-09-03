4402f5d50b46499b69c8a00ed079eb05bf991e9d
kirke <kirke@kirkenoMacBook-Air.local>
2022-09-03 15:51:27 +0900

    create memo list

--- app/root.tsx
+++ app/root.tsx
@@ -14,13 +14,13 @@ export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Minerva",
   viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <Meta />
         <Links />
