a05ba41a4a19bbe575ee4abe714d9b46653d28e5
2814109 <2814109k@gmail.com>
2022-10-01 23:44:48 +0900

    delete unnecessary code

--- app/routes/signin.tsx
+++ app/routes/signin.tsx
@@ -1,49 +1,26 @@
-import { useTransition, useActionData } from "@remix-run/react";
+import { useActionData } from "@remix-run/react";
 import {
   ActionFunction,
   LoaderFunction,
   redirect,
   json,
 } from "@remix-run/node";
-// import LoginedComponent from "~/componets/routes/auth/LoginedComponent";
-// import SigninButton from "~/componets/routes/auth/SigninButton";
 import SignInForm from "~/componets/form/SignInForm";
 import { FC } from "react";
-// import useRealm from "~/hooks/realmClient";
-// import { SubmitHandler } from "react-hook-form";
 import { createUser } from "~/models/auth.server";
 import { UserDetails } from "~/types/models/UserDetails";
-// import { UserDetails } from "~/types/models/UserDetails";
 
 export const loader: LoaderFunction = async () => {
   return process.env.ATLAS_APP_SERVICE;
 };
 
-type Inputs = {
-  username: string;
-  email: string;
-};
 const SignIn: FC = () => {
-  const data = useActionData();
-  console.log(data);
-
-  //   const ATLAS_APP_SERVICE = useLoaderData();
-
-  //   const { clientRealm, register } = useRealm(ATLAS_APP_SERVICE);
-
-  const transition = useTransition();
-  const isTransition = transition.submission;
+  const errors = useActionData();
 
   return (
     <div className="bg-gray-200">
       <>
-        {/* {currentUser ? (
-            <LoginedComponent currentUser={currentUser} />
-          ) : (
-            //   <SigninButton />
-            <SignInForm />
-          )} */}
-        <SignInForm errors={data} />
+        <SignInForm errors={errors} />
       </>
     </div>
   );
