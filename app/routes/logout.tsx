5f247dd0aed2cd8151631e080d0bbc86e4ce7d13
2814109 <2814109k@gmail.com>
2022-10-02 14:24:48 +0900

    impl log out and delete session at cookie

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,8 +1,14 @@
 import { ActionFunction } from "@remix-run/server-runtime";
 import { redirect } from "@remix-run/node";
 import { logout } from "~/models/auth.server";
-
-export const action: ActionFunction = async () => {
+import { getSession, destroySession } from "~/utils/sessions.server";
+export const action: ActionFunction = async ({ request }) => {
   await logout();
-  return redirect("/login");
+  const session = await getSession(request.headers.get("Cookie"));
+  if (session.has("access_token")) {
+    return redirect("/login", {
+      headers: { "Set-Cookie": await destroySession(session) },
+    });
+  }
+  return redirect("/");
 };
